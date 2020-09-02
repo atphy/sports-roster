ec3112a967cb039111e1b0baea8a9872c3537f0b
Austin Phy <atphy42@gmail.com>
2020-09-01 20:27:56 -0500

    data added

--- src/App/App.js
+++ src/App/App.js
@@ -5,9 +5,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <h3 className="top-header">Who the puck are the</h3>
         <h1 className="title">Dayton Gems</h1>
-        <h2 className="sub-header">1964-1980</h2>
+        <h2 className="sub-header hidden">1964-1980</h2>
         <button className="login-button">Log In</button>
+        <img className="hidden" src="https://i.imgur.com/g2rCa5I.png"></img>
       </div>
     );
   }
