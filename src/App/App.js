0f2c306f2766f2e434eb4f5690aadb5a3ee71dad
Austin Phy <atphy42@gmail.com>
2020-09-04 12:57:37 -0500

    auth works

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,49 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import './App.scss';
 
-import Auth from '../components/Auth';
+import fbConnection from '../helpers/data/connection';
 
+import Auth from '../components/Auth/Auth';
+import CardContainer from '../components/CardContainer/CardContainer';
+
+fbConnection();
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+
+    const loadComponent = () => {
+      if (authed) {
+        return <CardContainer authed={authed} />;
+      }
+      return <Auth />;
+    };
+
     return (
       <div className="App">
         <h3 className="top-header">Who the puck are the</h3>
         <h1 className="title">Dayton Gems</h1>
-        <Auth />
+        {loadComponent()}
       </div>
     );
   }
--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import './Auth.scss';
 
 class Auth extends React.Component {
+  loginClick = (e) => {
+    e.preventDefault();
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(googleProvider);
+  }
+
   render() {
     return (
         <div className="auth">
           <h2 className="sub-header hidden">1964-1980</h2>
-          <button className="login-button">Log In</button>
+          <button className="login-button" onClick={this.loginClick}>Log In</button>
           <img className="login-img" src="https://i.imgur.com/g2rCa5I.png"></img>
         </div>
     );
