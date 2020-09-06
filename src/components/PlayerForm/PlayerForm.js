2d69e79d96efc43c801eb5717dfe7c24e816a432
Austin Phy <atphy42@gmail.com>
2020-09-06 14:11:03 -0500

    partial form formatting

--- src/components/PlayerForm/PlayerForm.js
+++ src/components/PlayerForm/PlayerForm.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import './PlayerForm.scss';
+
 class PlayerForm extends React.Component {
   render() {
     return (
-            <div className="player-form"><h1>player form</h1></div>
+            <div className="player-form">
+                <form>
+                  <div className="form-group">
+                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Name" required />
+                  </div>
+                  <div className="form-group">
+                     <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Position" required />
+                  </div>
+                  <div class="custom-file">
+                    <input type="file" class="custom-file-input" id="coaster-image" required />
+                    <label className="custom-file-label" for="coaster-image" id="coaster-image-label">Upload Player Image</label>
+                  </div>
+                </form>
+            </div>
     );
   }
 }
