954c54471de809336689f38c1a39ce1d1e897a0c
Austin Phy <atphy42@gmail.com>
2020-09-06 13:42:10 -0500

    delete works, started progress on create

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -8,40 +8,58 @@ import './CardContainer.scss';
 
 import playersData from '../../helpers/data/playersData';
 import Card from '../Card/Card';
+import PlayerForm from '../PlayerForm/PlayerForm';
 
 class CardContainer extends React.Component {
 state = {
   players: [],
+  formOpen: false,
 }
 
 static propTypes = {
   authed: PropTypes.bool.isRequired,
 }
 
-componentDidMount() {
+getPlayers = () => {
   playersData.getPlayersByUid(authData.getUid())
     .then((players) => this.setState({ players }))
     .catch((err) => console.error('get players broke', err));
+}
+
+componentDidMount() {
+  this.getPlayers();
 }
 
     logoutClick = (e) => {
       firebase.auth().signOut();
     }
 
+    deletePlayer = (playerId) => {
+      playersData.deletePlayer(playerId)
+        .then(() => {
+          this.getPlayers();
+        })
+        .catch((err) => console.error('delete failed', err));
+    }
+
     render() {
       const { authed } = this.props;
-      const { players } = this.state;
+      const { players, formOpen } = this.state;
 
-      const playerCard = players.map((player) => <Card key={player.name} player={player} />);
+      const playerCard = players.map((player) => <Card key={player.name} player={player} deletePlayer={this.deletePlayer} />);
 
       return (
+          <div>
+              <button className="open-form-button" onClick={() => { this.setState({ formOpen: !formOpen }); }}><i class="fas fa-plus-square"></i></button>
           <div className="card-container">
               {
                   authed
                     ? <button className="logout-button" onClick={this.logoutClick} >Log Out</button>
                     : ''
               }
+              { formOpen ? <PlayerForm /> : '' }
               {playerCard}
+          </div>
           </div>
       );
     }
---
+++ src/components/PlayerForm/PlayerForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class PlayerForm extends React.Component {
+  render() {
+    return (
+            <div className="player-form"><h1>player form</h1></div>
+    );
+  }
+}
+
+export default PlayerForm;
